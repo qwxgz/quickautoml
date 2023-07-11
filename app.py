9b6df0d572a7c32eea817a48b9d525991ba63b3e
Ofcoursewx <qiuwxgz@live.com>
2023-07-11 15:48:01 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import pycaret as pc
 import os
 
 # Import Profiling capabilities
@@ -13,16 +12,16 @@ from pycaret.classification import setup, compare_models, pull, save_model
 
 with st.sidebar:
     st.image("https://tse2-mm.cn.bing.net/th/id/OIP-C.ql7qtUlFZWrq2MbK72jPFwHaFQ?pid=ImgDet&rs=1")
-    st.title("AutoStreamML")
-    choice = st.radio("Navigation", ["Upload", "Profiling", "AutoML", "Download"])
-    st.info("This is a AutoML pipeline using Streamlit, Pandas Profiling, Pycaret and very easy to use!")
+    st.title("自动化机器学习 AutoStreamML")
+    choice = st.radio("功能浏览 Navigation", ["Upload", "Profiling", "AutoML", "Download"])
+    st.info("该项目可运行于网页端，通过建立自动机器学习AutoML管道，能够快速对数据特征进行自动化分析和建模分析，非常方便和快捷。- drafted by qwx")
 # st.write("Hello world!")
 
 if os.path.exists("sourcedata.csv"):
     df = pd.read_csv("sourcedata.csv", index_col=None)
 
 if choice == "Upload":
-    st.title("Upload Your Data for AutoML")
+    st.title("上传数据 Upload Data for AutoML")
     file = st.file_uploader("Upload Your Dataset from Button below")
     if file:
         df = pd.read_csv(file, index_col=None)
@@ -31,29 +30,30 @@ if choice == "Upload":
         pass
 
 if choice == "Profiling":
-    st.title("AutoEDA - Exploratory Data Analysis")
+    st.title("自动化EDA数据探索分析 Exploratory Data Analysis")
     profile_report = df.profile_report()
     st_profile_report(profile_report)
     pass
 
 if choice == "AutoML":
-    st.title("Machine Learning go ")
+    st.title("启动自动机器学习 Launch Auto Machine Learning")
     target = st.selectbox("Select Your Target", df.columns)
-    if st.button("Train AutoML Models"):
+    if st.button("训练AutoML模型 Train AutoML Models"):
+        st.info("模型训练中，请等待......Traing in Progress ")
         setup(df, target=target)
         setup_df = pull()  # check this
-        st.info("The AutoML Settings to Be Used:")
+        st.info("自动机器学习的设置参数 AutoML Settings to Be Used:")
         st.dataframe(setup_df)
         best_model = compare_models()
         compare_df = pull()
-        st.info("These are the Models Used in AutoML")
+        st.info("AutoML所用的模型 These are the Models Used in AutoML")
         st.dataframe(compare_df)
-        st.info("The Best Model is: ")
+        st.info("最佳模型 The Best Model is: ")
         st.info(best_model)
         save_model(best_model, 'best_model')
     pass
 
 if choice == "Download":
-    with open("best_model.pkl",'rb') as f:
-        st.download_button("Download the Model", f, "trained_ml_model.pkl")
+    with open("best_model.pkl", 'rb') as f:
+        st.download_button("下载模型 Download the Model", f, "trained_ml_model.pkl")
     pass
